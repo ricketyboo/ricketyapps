7a0ee32d821d885ca4c54bc63692e1c6cd6b05bd
ricketyboo <4494839+ricketyboo@users.noreply.github.com>
2025-06-11 00:55:36 +1000

    dummy success/fail on try_login

--- app/src/core/auth/mod.rs
+++ app/src/core/auth/mod.rs
@@ -20,26 +20,33 @@ pub struct Credentials {
 }
 
 #[server]
-pub async fn try_login(username:String, password:String) -> Result<(), ServerFnError> {
-    log!("try_login {username},{password}");
-    Ok(())
+pub async fn try_login(credentials: Credentials) -> Result<(), ServerFnError> {
+    log!("try_login {:?}", credentials);
+    if credentials.password == "correct" {
+        return Ok(())
+    }
+    Err(ServerFnError::Response("Invalid credentials".into()))
 }
 
 #[component]
 pub fn Login() -> impl IntoView {
-    let submit = ServerAction::<TryLogin>::new();
+    let submit_action = ServerAction::<TryLogin>::new();
+    // todo: how to get this? want to respond to the error. Maybe this isn't possible client side with ActionForm
+    //  https://docs.rs/reactive_graph/0.2.2/reactive_graph/actions/struct.Action.html
+    // let v = submit_action.value();
     let validate = move |event: SubmitEvent| {
+        // this is kind of making the ActionForm action redundant; but if I don't manual dispatch I can't get the credentials to unwrap properly?
+        event.prevent_default();
         let data = Credentials::from_event(&event);
         log!("{:?}", data);
         match data {
             Ok(credentials) => {
                 if credentials.username.is_empty() || credentials.password.is_empty() {
-                    log!("Invalid data");
-                    event.prevent_default();
+                    log!("Invalid data");                    
                 }
-                // else {
-                //     submit.dispatch(TryLogin::from(credentials));
-                // }
+                else {                   
+                    submit_action.dispatch(TryLogin::from(credentials));
+                }
             }
             Err(..) => {
                 // can check deserialisation errors here
@@ -50,7 +57,7 @@ pub fn Login() -> impl IntoView {
     };
     view! {
         <h2>"Login"</h2>
-        <ActionForm action=submit on:submit:capture=validate>
+        <ActionForm action=submit_action on:submit:capture=validate>
             <label>"username"<input name="username" /></label>
             <label>"password"<input name="password" type="password" /></label>
             <button>Login</button>
