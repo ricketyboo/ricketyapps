177d4e4394e325279d676854b45ae71c85398df9
ricketyboo <4494839+ricketyboo@users.noreply.github.com>
2025-06-11 00:55:35 +1000

    move db config into separate lib

---
+++ lib/db/src/lib.rs
@@ -0,0 +1,49 @@
+use secrecy::{ExposeSecret, SecretString};
+use sqlx::postgres::PgPoolOptions;
+use sqlx::{Error, Pool, Postgres};
+use std::env;
+
+#[derive(Clone)]
+pub struct DatabaseConfiguration {
+    pub username: String,
+    pub password: SecretString,
+    pub host: String,
+    pub port: u16,
+    pub name: String,
+}
+
+impl DatabaseConfiguration {
+    pub fn from_env() -> Self {
+        Self {
+            username: env::var("DB_USERNAME").expect("Unable to load DB_USERNAME"),
+            password: env::var("DB_PASSWORD")
+                .expect("Unable to load DB_PASSWORD")
+                .into(),
+            host: env::var("DB_HOST").expect("Unable to load DB_HOST"),
+            port: env::var("DB_PORT")
+                .expect("Unable to load DB_PORT")
+                .parse::<u16>()
+                .expect("unable to convert DB_PORT to int"),
+            name: env::var("DB_NAME").expect("Unable to load DB_NAME"),
+        }
+    }
+
+    pub fn connection_string(&self) -> String {
+        format!(
+            "postgres://{}:{}@{}:{}/{}",
+            self.username,
+            self.password.expose_secret(),
+            self.host,
+            self.port,
+            self.name,
+        )
+    }
+}
+
+pub async fn get_pool() -> Result<Pool<Postgres>, Error> {
+    let db_config = DatabaseConfiguration::from_env();
+    PgPoolOptions::new()
+        .max_connections(5)
+        .connect(&db_config.connection_string())
+        .await
+}
