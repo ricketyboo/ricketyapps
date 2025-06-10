49cc19934a7f75e1061b504b407e55d746e37499
ricketyboo <4494839+ricketyboo@users.noreply.github.com>
2025-06-11 00:55:36 +1000

    commented attempts at error handling

--- src/app/auth/register.rs
+++ src/app/auth/register.rs
@@ -25,12 +25,38 @@ pub async fn try_register(credentials: Credentials) -> Result<User, ServerFnErro
 #[component]
 pub fn Register() -> impl IntoView {
     let submit_action = ServerAction::<TryRegister>::new();
+    // let value = Signal::derive(move || {
+    //     submit_action
+    //         .value()
+    //         .get()
+    //         .unwrap_or_else(|| Ok(User::default()))
+    // });
+    // Effect::new_isomorphic(move |_| {
+    //     log!("Got value = {:?}", value.get());
+    // });
     view! {
         <h2>"Register"</h2>
+        // <ErrorBoundary fallback=move |error| {
+        // view! {
+        // <p>Error</p>
+        // <ul>
+        // {move || {
+        // error
+        // .get()
+        // .into_iter()
+        // .map(|(_, e)| view! { <li>{e.to_string()}</li> })
+        // .collect::<Vec<_>>()
+        // }}
+        // 
+        // </ul>
+        // }
+        // }>
+        // <pre>{move || value.get().unwrap().id.to_string()}</pre>
         <ActionForm action=submit_action>
             <label>"username"<input name="credentials[username]" /></label>
             <label>"password"<input name="credentials[password]" type="password" /></label>
             <button type="submit">Register</button>
         </ActionForm>
+        // </ErrorBoundary>
     }
 }
