49c1905f32fa9ad95d4d38a050a5b647ceb886aa
ricketyboo <4494839+ricketyboo@users.noreply.github.com>
2025-06-15 17:58:27 +1000

    add initial weld model derives. Need to setup relationships still.

--- src/app/planner/entity/mod.rs
+++ src/app/planner/entity/mod.rs
@@ -1,6 +1,6 @@
+use super::super::auth::entity::user::UserRow;
 use uuid::Uuid;
 use welds::WeldsModel;
-use super::super::auth::entity::user::UserRow;
 #[derive(Debug, WeldsModel)]
 #[welds(table = "trips")]
 #[welds(BelongsTo(owner, UserRow, "owner_id"))]
@@ -11,4 +11,43 @@ pub struct Trip {
     pub(crate) owner_id: Uuid,
     pub(crate) start_at: Option<chrono::DateTime<chrono::Utc>>,
     pub(crate) end_at: Option<chrono::DateTime<chrono::Utc>>,
+}
+
+#[derive(Debug, WeldsModel)]
+#[welds(table = "addresses")]
+pub struct Address {
+    #[welds(primary_key)]
+    id: Uuid,
+    owner_id: Uuid,
+    line1: String,
+    line2: Option<String>,
+    line3: Option<String>,
+    province: Option<String>,
+    city: Option<String>,
+    postcode: Option<String>,
+    country: Option<String>,
+    created_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
+
+#[derive(Debug, WeldsModel)]
+#[welds(table = "accommodations")]
+pub struct Accommodation {
+    #[welds(primary_key)]
+    id: Uuid,
+    owner_id: Uuid,
+    name: String,
+    address_id: String,
+    created_at: chrono::DateTime<chrono::Utc>,
+    updated_at: chrono::DateTime<chrono::Utc>,
+}
+#[derive(Debug, WeldsModel)]
+#[welds(table = "trip_accommodation_booking")]
+pub struct TripAccommodationBooking {
+    #[welds(primary_key)]
+    id: Uuid,
+    trip_id: Uuid,
+    accommodation_id: Uuid,
+    check_in: chrono::DateTime<chrono::Utc>,
+    check_out: chrono::DateTime<chrono::Utc>,
 }
