968f4cf702b8431b2aa6378760283dd414435c5b
ricketyboo <4494839+ricketyboo@users.noreply.github.com>
2025-06-15 17:51:00 +1000

    let postgres auto gen FK constraint keys

--- migrations/20250614055442_create_trips_table.up.sql
+++ migrations/20250614055442_create_trips_table.up.sql
@@ -2,12 +2,11 @@ CREATE TABLE trips
 (
     id         UUID PRIMARY KEY     DEFAULT gen_random_uuid(),
     name       VARCHAR     NOT NULL,
-    owner_id   UUID        NOT NULL,
+    owner_id   UUID        NOT NULL REFERENCES users (id) ON DELETE CASCADE,
     start_at TIMESTAMPTZ NULL,
     end_at TIMESTAMPTZ NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    updated_at TIMESTAMPTZ NULL,
-    CONSTRAINT fk_owner FOREIGN KEY (owner_id) REFERENCES users (id) ON DELETE CASCADE
+    updated_at TIMESTAMPTZ NULL
 );
 
 CREATE TRIGGER set_updated_at
